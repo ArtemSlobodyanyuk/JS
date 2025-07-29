4d387fb3baa9e65085e9de3d0d3dfb244115eb2c
ArtemSlobodyanyuk <slobodyanyukau@gmail.com>
2025-07-29 18:37:28 +0300

    A

--- D1.js
+++ D1.js
@@ -1,6 +1,6 @@
 let carcas = {name: "Artem", age:17, student:true, weight:70.3, country:"Ukreine"};
 console.log(carcas)
 
-let a=5, b=10;
+let a=10, b=10;
 let c = [a+b, a-b, a*b, a/b, a%b]
 console.log(c)
