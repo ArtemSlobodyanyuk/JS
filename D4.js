616d142dc715218058efebb80508d775a043af72
ArtemSlobodyanyuk <slobodyanyukau@gmail.com>
2025-08-26 18:24:33 +0300

    t4

--- D4.js
+++ D4.js
@@ -4,24 +4,50 @@ let arr = document.getElementById("arr");
 let p = document.getElementById("p");
 let todos = [];
 
-btn.addEventListener("click", function () {
-    let newLi = document.createElement("li");
-
-    if (task.value.trim() === "") {p.textContent = "Введіть завдання!";} 
-    else {
-        p.textContent = "";
-        newLi.textContent = task.value;
-        todos.push({Text: task.value, done: false});
-        let index = todos.length - 1;
-        newLi.dataset.index = index;
-        arr.appendChild(newLi);
+btn.addEventListener("click", function () { // todo-список
+    if (task.value.trim() === "") {         // Якщо рядок пустий
+        p.textContent = "Введіть завдання!";
+        return;
+    } 
         
-        newLi.addEventListener("click", function () {
-            let idx = Number(newLi.dataset.index);
-            newLi.classList.toggle("done");
-            todos[idx].done = !todos[idx].done;
+    p.textContent = "";
+    console.log(todos);
 
-            console.log(todos);
-        })}
+    let todo = {text: task.value, done: false};
+    todos.push(todo);
+    let index = todos.length - 1;
+
+    let newLi = document.createElement("li");
+    newLi.textContent = todo.text;
+    newLi.dataset.index = index;
+        
+    let doneBtn = document.createElement("button");
+    doneBtn.textContent = "✔ Завершити";
+    doneBtn.addEventListener("click", function () {
+        let idx = Number(newLi.dataset.index);
+        todos[idx].done = !todos[idx].done;
+
+        if (todos[idx].done) {
+            newLi.style.textDecoration = "line-through";
+        } else {
+            newLi.style.textDecoration = "none";
+        }
+        console.log(todos);
+    });
+    let delBtn = document.createElement("button");
+    delBtn.textContent = "🗑 Видалити";
+    delBtn.addEventListener("click", function () {
+        let idx = Number(newLi.dataset.index);
+        todos.splice(idx, 1); 
+        newLi.remove();       
+        console.log(todos);
+    });
+
+    
+    newLi.appendChild(doneBtn);
+    newLi.appendChild(delBtn);
+
+
+    arr.appendChild(newLi);
     task.value = "";
 });
